28fdbaf647a332da183b7426d8cf29301ec9e238
Jeremy Walton <jthelegoman@gmail.com>
2014-02-27 20:52:19 -0500

    Finished chapter 9.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  before_action :unsigned_in_user, only: [:new, :create]
 
   def show
     @user = User.find(params[:id])
   end
   def new
     @user = User.new
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
   def create
     @user = User.new(user_params)
@@ -53,8 +60,18 @@ class UsersController < ApplicationController
       end
     end
 
+    def unsigned_in_user
+      unless !signed_in?
+        redirect_to root_url, notice: "You already have an account."
+      end
+    end
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,28 @@ describe "User pages" do
         end
       end
     end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+
   end
 
   describe "profile page" do
@@ -60,7 +82,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -84,6 +106,18 @@ describe "User pages" do
       before do
         sign_in user
         visit edit_user_path(user)
+      end
+
+      describe "forbidden attributes" do
+        let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+        end
+        before do
+          sign_in user, no_capybara: true
+          patch user_path(user), params
+        end
+        specify { expect(user.reload).not_to be_admin }
       end
 
       describe "page" do
